ec7c3078a499ff44345b95267359ae3d1adfc565
Ehsan-Aghapour <aghapour.ehsan17@gmail.com>
2021-03-22 12:50:09 +0000

    src/runtime/IScheduler.cpp which is interface scheduler and parent of schedulers

--- src/runtime/IScheduler.cpp
+++ src/runtime/IScheduler.cpp
@@ -37,6 +37,11 @@ IScheduler::IScheduler()
     utils::cpu::get_cpu_configuration(_cpu_info);
     // Work out the best possible number of execution threads
     _num_threads_hint = utils::cpu::get_threads_hint();
+
+
+	//Ehsan
+	//std::cout<<"IScheduler.cpp, cpu_num: "<<_cpu_info.get_cpu_num()<<" _num_threads_hint: "<<_num_threads_hint<<std::endl;
+	
 }
 
 CPUInfo &IScheduler::cpu_info()
