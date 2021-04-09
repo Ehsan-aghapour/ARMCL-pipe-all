4152466d5977d0d38934902a4e46af761f343825
Ehsan-aghapour <aghapour.ehsan17@gmail.com>
2021-04-09 02:40:45 +0200

    without standalone

---
+++ b.sh
@@ -0,0 +1,9 @@
+
+compiler=arm-linux-androideabi-clang++
+target=armv7a-linux-androideabi$1-clang++
+p=../android-ndk-r21e-linux-x86_64/android-ndk-r21e/toolchains/llvm/prebuilt/linux-x86_64/bin/
+cp $p/$target $p/$compiler
+
+XX=clang++ CC=clang scons Werror=0 -j16 debug=0 asserts=0 neon=1 opencl=1 os=android arch=armv7a 
+
+rm $p/$compiler
